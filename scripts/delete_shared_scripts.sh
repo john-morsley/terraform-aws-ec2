89a5cad842d794eefff64c1b9e249c108401ea56
John Morsley <johnmorsley@gmail.com>
2020-05-21 12:20:43 +0100

    Refactoring

--- scripts/delete_shared_scripts.sh
+++ scripts/delete_shared_scripts.sh
@@ -2,8 +2,6 @@
 
 echo 'Cloning Shared Scripts repository...'
 
-echo "SHARED_SCRIPTS_FOLDER: ${SHARED_SCRIPTS_FOLDER}"
-
-git clone https://github.com/john-morsley/shared-scripts.git
+rm --recursive --force shared-scripts-*
 
 exit 0
