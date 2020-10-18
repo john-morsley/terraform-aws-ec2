b13ec42af316dbd5a7e95c3757127c1e4e92d5af
John Morsley <johnmorsley@gmail.com>
2020-10-18 15:06:30 +0100

    Removed private subnet

--- examples/docker/variables.tf
+++ examples/docker/variables.tf
@@ -37,7 +37,7 @@ variable "public_subnet_cidr" {
   //default = ["10.0.1.0/24"] # 251 (256 possible - 5 reserved by AWS) 
 }
 
-variable "private_subnet_cidr" {
-  type = list(string)
-  //default = ["10.0.2.0/24"] # 251 (256 possible - 5 reserved by AWS) 
-}
+//variable "private_subnet_cidr" {
+//  type = list(string)
+//  default = ["10.0.2.0/24"] # 251 (256 possible - 5 reserved by AWS) 
+//}
